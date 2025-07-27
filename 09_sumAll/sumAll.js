15c9332a48d4fdce7fa58931c6595763f0b8ed5b
AmericanNewb <americannewb@gmail.com>
2025-07-27 11:38:57 -0400

    uploading sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(a , b) {
+    if(a < 0 || b < 0) return 'ERROR' 
+    if (typeof a !== 'number' || typeof b !== 'number') return 'ERROR';
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return 'ERROR';
+    let array = [];
+    if(b > a){
+        for (let i = a; i <= b; i ++){
+        array.push(i);
+    }
+    } else if(a > b) {
+        for (let i = a; i >= b; i --){
+        array.push(i);
+    }
+    }
+    return array.reduce((acc, ele) => acc + ele)
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
