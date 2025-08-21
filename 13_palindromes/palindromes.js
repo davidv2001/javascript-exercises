7a1da88f9d5f12fa9e996942fad8d4f5644abd32
David Valencia <davidv2001learning@gmail.com>
2025-08-21 13:12:48 -0400

    13-palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  // Normalize the string: lowercase + remove non-alphanumeric
+  let cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
 
+  // Reverse the cleaned string
+  let reverseStr = "";
+  for (let i = cleanStr.length - 1; i >= 0; i--) {
+    reverseStr += cleanStr[i];
+  }
+
+  return reverseStr === cleanStr;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
