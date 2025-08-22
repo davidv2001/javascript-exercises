564bd6547847652c04eb834667e709ab990c59c5
David Valencia <davidv2001learning@gmail.com>
2025-08-22 09:50:29 -0400

    fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  let count = [];
+  if (num < 0) return `OOPS`;
+  if (typeof num === "string") +num;
+  for (let i = 0; i <= num; i++) {
+    if (i === 0 || i === 1) count.push(i);
+    else {
+      count.push(count[i - 1] + count[i - 2]);
+    }
+  }
+  return count[num];
 };
 
 // Do not edit below this line
