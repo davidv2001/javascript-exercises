1fa72b9cccf45c99b42f56eb05c8bf7daa667b09
David Valencia <davidv2001learning@gmail.com>
2025-08-22 12:55:10 -0400

    16-findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  let highest = 0;
+  let index = 0;
 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].yearOfDeath === undefined) {
+      if (2025 - arr[i].yearOfBirth > highest) {
+        highest = 2025 - arr[i].yearOfBirth;
+        index = i;
+      }
+    }
+    console.log(arr[i].yearOfDeath);
+    if (arr[i].yearOfDeath - arr[i].yearOfBirth > highest) {
+      highest = arr[i].yearOfDeath - arr[i].yearOfBirth;
+      index = i;
+    }
+  }
+
+  return arr[index];
 };
 
 // Do not edit below this line
